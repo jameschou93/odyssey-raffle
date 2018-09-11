e4fd3ba0ef88a3c675f0754c489b616a28692b84
James Chou <james.chou.career@gmail.com>
2018-09-11 18:57:34 -0400

    updated exercises

--- app/controllers/raffle_entries_controller.rb
+++ app/controllers/raffle_entries_controller.rb
@@ -14,7 +14,9 @@ class RaffleEntriesController < ApplicationController
     @scores = RaffleEntry.where("created_at > ?", 1.days.ago.beginning_of_day).map {|n| n.repetitions.to_i}.sum
     @pushups = RaffleEntry.where(exercise: "Push-Ups").map {|n| n.repetitions.to_i}.sum
     @pullups = RaffleEntry.where(exercise: "Pull-Ups").map {|n| n.repetitions.to_i}.sum
-    @situps = RaffleEntry.where(exercise: "Sit-Ups").map {|n| n.repetitions.to_i}.sum
+    @crunches = RaffleEntry.where(exercise: "Crunches").map {|n| n.repetitions.to_i}.sum
+    @lunges = RaffleEntry.where(exercise: "Lunges").map {|n| n.repetitions.to_i}.sum
+    @squats = RaffleEntry.where(exercise: "Squats").map {|n| n.repetitions.to_i}.sum
   end
 
   def raffle_results
